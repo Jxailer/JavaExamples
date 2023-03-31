e73e1fd0ed0a34b3dc1acfab990156104ba7f2b4
deser <deser@DESKTOP-EPV74RL>
2023-03-31 12:59:56 +0900

    fist commit (if statement of control statement)

---
+++ CH04_EX01_2005872/src/Test.java
@@ -0,0 +1,101 @@
+public class Test {
+	public static void main(String[] args) {
+		//if 제어문
+		
+		//중괄호의 생략
+		int a=3;
+		if(a>5) {
+			System.out.println("실행1");
+			System.out.println("실행2");
+		}	
+		// a>5가 false이므로 아무것도 실행되지 않음.
+		
+		if(a>5) 
+			System.out.println("실행3"); // 중괄호 포함
+			System.out.println("실행4"); // 중괄호 미포함. 출력됨.
+			
+			
+		//type1
+		if(a<4) {
+			System.out.println("실행5");
+		}
+		if(a<2) {
+			System.out.println("실행6");
+		}
+		
+		//type 2
+		// ex1
+		int b;
+		if(a>2) {
+			b = 10;
+		}
+		else {
+			b = 20;
+		}
+		// == (a>2)?b=10:b=20;
+		
+		// ex2
+		if(a>2) {
+			System.out.println("실행7");
+		}
+		else {
+			System.out.println("실행8"); // a>2가 참이므로 출력되지 않음
+		}
+		
+		// type 3
+		// ex 1
+		int score = 85;
+		
+		if(score>=90) {
+			System.out.println("A");
+		}
+		else if(score>=80) {
+			System.out.println("B");
+		}
+		else if(score>=70) {
+			System.out.println("C");
+		}
+		else {
+			System.out.println("F");
+		}
+		
+		// ex2
+		if(s>=70) {
+			System.out.println("C학점");
+		}
+		else if(s>=80) {
+			System.out.println("B학점");
+		}
+		else if(s>=90) {
+			System.out.println("A학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+
+		// ex3
+
+		if(70<=s && s<80) {
+			System.out.println("C학점");
+		}
+		else if(80<=s && s<90) {
+			System.out.println("B학점");
+		}
+		else if(s>=90) {
+			System.out.println("A학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
