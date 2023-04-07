46198a23a4c6a913627807ef5c95bb98e2fe5967
Jxailer <jacksonxailer@gmail.com>
2023-04-07 12:35:43 +0900

    first commit(while and do-while statement)

---
+++ CH04_EX07_2005872/src/Test.java
@@ -0,0 +1,46 @@
+
+public class Test {
+	public static void main (String [] ar) {
+		// while <=> do-while
+		int a;
+		
+		a=0;
+		while(a<3) {
+			System.out.println(a);
+			a++;
+		} // 3 times
+		
+		a=0;
+		do {
+			System.out.println(a);
+			a++;
+		}while(a<3); // 3 times
+		
+		a=0;
+		while(a<1) {
+			System.out.println(a);
+			a++;
+		} // 1 time
+		
+		a=0;
+		do {
+			System.out.println(a);
+			a++;
+		}while(a<1); // 1 time		
+		
+		a=0;
+		while(a<0) {
+			System.out.println(a);
+			a++; // 1 time
+		}		
+		a=0;
+		do {
+			System.out.println(a);
+			a++;
+		} while(a<0); // 0 time
+		
+		
+		
+		
+	}
+}
